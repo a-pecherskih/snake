96316008420ad61b5f2fb1ab8367cac06a2d5b33
a-pecherskih <pecherskih.ulstu@mail.ru>
2018-12-29 18:25:35 +0400

    add new classes

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,32 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2 = p1;
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine RightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            RightLine.Draw();
 
-            Console.WriteLine("p2.x = " + p2.x);
-            Console.WriteLine("p2.y = " + p2.y);
-            Console.WriteLine("p2.sym = " + p2.sym);
-            Console.WriteLine();
-            //p2.Draw();
-
-            Console.WriteLine("p1.x = " + p1.x);
-            Console.WriteLine("p1.y = " + p1.y);
-            Console.WriteLine("p1.sym = " + p1.sym);
-            //p1.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
 
             
-        }
-
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x += dx;
-            p.y += dy;
         }
 
     }   
